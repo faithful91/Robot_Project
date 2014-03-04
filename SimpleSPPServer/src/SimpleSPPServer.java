3acb242116a1a7dc9c34c5e1fa6263e846fddcde
faithful91 <bradai.hazem@gmail.com>
2014-03-04 04:17:26 +0100

    BT ACC pb all in one

--- SimpleSPPServer/src/SimpleSPPServer.java
+++ SimpleSPPServer/src/SimpleSPPServer.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -8,48 +10,57 @@ import java.io.PrintWriter;
   
 import javax.bluetooth.*;
 import javax.microedition.io.*;
+
   
 /**
 * Class that implements an SPP Server which accepts single line of
 * message from an SPP client and sends a single line of response to the client.
 */
 public class SimpleSPPServer {
-    
     //start server
     private void startServer() throws IOException{
-  
+        StreamConnection conn = null;
+        DataInputStream inStream=null;
+        BufferedReader bReader=null;
+    	
         //Create a UUID for SPP
         UUID uuid = new UUID("1101", true);
         //Create the servicve url
         String connectionString = "btspp://localhost:" + uuid +";name=Sample SPP Server";
-        
+        StreamConnection connection=null;
+        while (true){
+        	try{
         //open server url
         StreamConnectionNotifier streamConnNotifier = (StreamConnectionNotifier)Connector.open( connectionString );
-        
         //Wait for client connection
         System.out.println("\nServer Started. Waiting for clients to connect...");
-        StreamConnection connection=streamConnNotifier.acceptAndOpen();
-  
+        
+        
+       connection=streamConnNotifier.acceptAndOpen();
         RemoteDevice dev = RemoteDevice.getRemoteDevice(connection);
         System.out.println("Remote device address: "+dev.getBluetoothAddress());
         System.out.println("Remote device name: "+dev.getFriendlyName(true));
         
         //read string from spp client
-        InputStream inStream=connection.openInputStream();
-        BufferedReader bReader=new BufferedReader(new InputStreamReader(inStream));
+         inStream=connection.openDataInputStream();
+        bReader=new BufferedReader(new InputStreamReader(inStream));
         String lineRead=bReader.readLine();
         System.out.println(lineRead);
         
-        //send response to spp client
-        OutputStream outStream=connection.openOutputStream();
-        PrintWriter pWriter=new PrintWriter(new OutputStreamWriter(outStream));
-        pWriter.write("Response String from SPP Server\r\n");
-        pWriter.flush();
-  
-        pWriter.close();
+
+       
         streamConnNotifier.close();
+        }catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}}
+       
+        
+        //send response to spp client
+        
   
-    }
+        
+        }
   
   
     public static void main(String[] args) throws IOException {
--- Test2/gen/com/example/test2/R.java
+++ Test2/gen/com/example/test2/R.java
@@ -24,8 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080004;
         public static final int out=0x7f080000;
+        public static final int x_axis=0x7f080001;
+        public static final int y_axis=0x7f080002;
+        public static final int z_axis=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
